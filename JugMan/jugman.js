0654175d302e520564a2f564d01decc6cb129a5a
AngelusPonmalar <158824610+AngelusPonmalar@users.noreply.github.com>
2024-07-09 19:05:57 +0530

    Add files via upload

---
+++ JugMan/jugman.js
@@ -0,0 +1,13 @@
+  // Select the side navbar element
+  var sidenav = document.querySelector(".side-navbar");
+
+  // Function to show the side navbar
+  function showNav() {
+      sidenav.style.right= "0%";
+  }
+
+  // Function to close the side navbar
+  function closeNav() {
+      sidenav.style.right = "-60%";
+  }
+  
